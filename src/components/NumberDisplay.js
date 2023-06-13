153beb6c827796c34c82bc5ff6561c3be878df90
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-13 18:39:27 +0100

    removing unnecessary css import from numberdisplay

---
+++ src/components/NumberDisplay.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class NumberDisplay extends React.Component {
+  render() {
+    const style = {
+      backgroundColor: "#ADD8E6",
+      color: "#00008B",
+      height: "60px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }
+
+    return (
+      <div className="m-2" style={style}>
+        {this.props.displayText}
+      </div>
+    )
+  }
+}
+
+export default NumberDisplay
