9bd6443288292c1ea3b3db63a9d81a1ac1cb135c
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-13 18:02:50 +0100

    Create NumberDisplay component

---
+++ src/components/NumberDisplay.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './NumberDisplay.css'
+
+class NumberDisplay extends React.Component {
+  render() {
+    const style = {
+      backgroundColor: "#ADD8E6",
+      color: "#00008B",
+      height: "60px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }
+
+    return (
+      <div className="m-2" style={style}>
+        {this.props.displayText}
+      </div>
+    )
+  }
+}
+
+export default NumberDisplay
